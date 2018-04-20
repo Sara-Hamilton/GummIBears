e171fff8b8b1570caf202a8a12ea29c9c7fe7ad6
Sara Hamilton <sara@indeterminate.net>
2018-04-20 16:17:34 -0700

    add product class

---
+++ GummiBears/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummiBearKingdom.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
+        public decimal? Cost { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
