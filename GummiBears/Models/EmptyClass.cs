f135d0616051222c127f090c7727ca4f86e8ecbf
Sara Hamilton <sara@indeterminate.net>
2018-04-20 16:13:20 -0700

    add GummiDbContext

---
+++ GummiBears/Models/EmptyClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GummiBearKingdom.Models
+{
+    public class GummiDbContext : DbContext
+    {
+        public GummiDbContext()
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(@"Server=localhost;Port=8889;database=gummi_bears;uid=root;pwd=root;");
+        }
+
+        public GummiDbContext(DbContextOptions<GummiDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Product>().ToTable("Products");
+            base.OnModelCreating(builder);
+        }
+    }
+}
