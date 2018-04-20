eabdf58116b2efa903e684e78ad2638ffd744cc5
Sara Hamilton <sara@indeterminate.net>
2018-04-20 16:46:41 -0700

    modify models

--- GummiBears/Models/Product.cs
+++ GummiBears/Models/Product.cs
@@ -16,6 +16,6 @@ namespace GummiBears.Models
         public string Description { get; set; }
         [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
         public decimal? Cost { get; set; }
-        public virtual ICollection<Product> Products { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
     }
 }
---
+++ GummiBears/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummiBears.Models
+{
+    [Table("Reviews")]
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Rating { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
