f0cd5a29b2cb36221e4a8af83a832fe0ef7f3969
Sara <sara@indeterminate.net>
2018-04-25 21:08:51 -0700

    add images

---
+++ GummiBears/Migrations/20180426034112_AddImageUrls.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GummiBears.Migrations
+{
+    public partial class AddImageUrls : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Products");
+        }
+    }
+}
