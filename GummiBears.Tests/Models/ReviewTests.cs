384c0df523c93215f79351257d51b3e483f2fe13
Kayla Ondracek and Stephanie Faber <kayla.renee@gmail.com and eyesicedover@gmail.com>
2018-04-27 10:55:37 -0700

    add failing test condition for review constructor

---
+++ GummiBears.Tests/Models/ReviewTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GummiBears.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GummiBears.Tests
+{
+    [TestClass]
+    public class ReviewTests
+    {
+        [TestMethod]
+        public void Constructor_CreatesAProductObject_True()
+        {
+            //Arrange
+            var review = new Review();
+
+            //Act
+
+            //Assert
+            Assert.IsInstanceOfType(review, typeof(Product));
+        }
+    }
+}
