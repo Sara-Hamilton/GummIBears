f39239df4542a63400270ad4885f4a5e0f0a9bd1
Kayla Ondracek and Stephanie Faber <kayla.renee@gmail.com and eyesicedover@gmail.com>
2018-04-27 11:33:41 -0700

    add product repositories

---
+++ GummiBears/Models/Repositories/EFProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GummiBears.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GummiBears.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        GummiDbContext db = new GummiDbContext();
+
+        public IQueryable<Product> Products
+        { get { return db.Products; } }
+
+        public Product Save(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return product;
+        }
+
+        public Product Edit(Product product)
+        {
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
+            return product;
+        }
+
+        public void Remove(Product product)
+        {
+            db.Products.Remove(product);
+            db.SaveChanges();
+        }
+    }
+}
