8d3d883b166ce3efa5f3cce9b45b0823ded34d00
Kayla Ondracek and Stephanie Faber <kayla.renee@gmail.com and eyesicedover@gmail.com>
2018-04-27 13:19:34 -0700

    Add mock setup to product controllers tests

--- GummiBears.Tests/ControllerTests/ProductsControllerTest.cs
+++ GummiBears.Tests/ControllerTests/ProductsControllerTest.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using GummiBears.Models;
+using GummiBears.Controllers;
+using Moq;
+using System.Linq;
+using System;
 
 namespace GummiBears.Tests.ControllerTests
 {
-    class ProductsControllerTes
+    [TestClass]
+    public class ProductsControllerTest
     {
+        Mock<IProductRepository> mock = new Mock<IProductRepository>();
+
+        private void DbSetup()
+        {
+            mock.Setup(m => m.Products).Returns(New Product[]
+            {
+                new Product { ProductId = 1, Name = "Giant Gummi", Description = "12 oz. gummi bear", Cost = 4.99m, ImageUrl = "https://i.ytimg.com/vi/1CbfG0epWHo/maxresdefault.jpg" },
+            new Product { ProductId = 2, Name = "Green Gummies", Description = "16 oz. bag  of green gummi bears", Cost = 6.49m, ImageUrl = "https://www.ilovesugar.com/images/Green-Apple-Gummy-Bears-Candy.jpg" },
+            new Product { ProductId = 3, Name = "Glitter Gummies", Description = "16 oz. bag  of pink and purple glittery gummi bears", Cost = 8, ImageUrl = "https://img0.etsystatic.com/165/1/8581691/il_340x270.1095861008_ny4h.jpg" },
+            }.AsQueryable());
+        }
+
+        ViewResult indexView = new ProductsControllerTest(mock.Object).Index() as ViewResult;
+        
     }
 }
