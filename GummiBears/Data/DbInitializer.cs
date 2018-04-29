6aec5f7e6ac3eed08a995665e26f712b7f09a5cd
Sara <sara@indeterminate.net>
2018-04-29 16:12:20 -0700

    add average rating to seeded data in db initializer

--- GummiBears/Data/DbInitializer.cs
+++ GummiBears/Data/DbInitializer.cs
@@ -17,12 +17,12 @@ namespace GummiBears.Data
 
             var products = new Product[]
             {
-                new Product(){ Name = "Giant Gummi", Description = "12 oz. gummi bear", Cost = 4.99m, ImageUrl = "https://i.ytimg.com/vi/1CbfG0epWHo/maxresdefault.jpg" },
-                new Product(){ Name = "Green Gummies", Description = "16 oz. bag  of green gummi bears", Cost = 6.49m, ImageUrl = "https://www.ilovesugar.com/images/Green-Apple-Gummy-Bears-Candy.jpg"},
-                new Product(){ Name = "Glitter Gummies", Description = "16 oz. bag  of pink and purple glittery gummi bears", Cost = 8, ImageUrl = "https://img0.etsystatic.com/165/1/8581691/il_340x270.1095861008_ny4h.jpg" },
-                new Product(){ Name = "Shark Gummies", Description = "8 0z. bag of shark gummies", Cost = 4, ImageUrl = "https://tse2.mm.bing.net/th?id=OIP.a-PODiqFhHX7pdJxcKuK6gHaFj&pid=15.1&P=0&w=236&h=178" },
-                new Product(){ Name = "Gummi Worms", Description = "16 oz. bag of rainbow colored gummi worms", Cost = 6, ImageUrl = "http://25.media.tumblr.com/tumblr_m3295w1I751qfz9ceo1_500.jpg" },
-                new Product(){ Name = "Red Hot Gummies", Description = "16 oz. bag  of red hot gummi bears", Cost = 3 , ImageUrl = "https://tse4.mm.bing.net/th?id=OIP.flaaR4C9-l9cdbIWbVletQHaHa&pid=15.1&P=0&w=300&h=300"}
+                new Product(){ Name = "Giant Gummi", Description = "12 oz. gummi bear", Cost = 4.99m, ImageUrl = "https://i.ytimg.com/vi/1CbfG0epWHo/maxresdefault.jpg", AverageRating = 5.0m },
+                new Product(){ Name = "Green Gummies", Description = "16 oz. bag  of green gummi bears", Cost = 6.49m, ImageUrl = "https://www.ilovesugar.com/images/Green-Apple-Gummy-Bears-Candy.jpg", AverageRating = 5.0m},
+                new Product(){ Name = "Glitter Gummies", Description = "16 oz. bag  of pink and purple glittery gummi bears", Cost = 8, ImageUrl = "https://img0.etsystatic.com/165/1/8581691/il_340x270.1095861008_ny4h.jpg", AverageRating = 3.0m },
+                new Product(){ Name = "Shark Gummies", Description = "8 0z. bag of shark gummies", Cost = 4, ImageUrl = "https://tse2.mm.bing.net/th?id=OIP.a-PODiqFhHX7pdJxcKuK6gHaFj&pid=15.1&P=0&w=236&h=178", AverageRating = 5.0m },
+                new Product(){ Name = "Gummi Worms", Description = "16 oz. bag of rainbow colored gummi worms", Cost = 6, ImageUrl = "http://25.media.tumblr.com/tumblr_m3295w1I751qfz9ceo1_500.jpg", AverageRating = 4.0m},
+                new Product(){ Name = "Red Hot Gummies", Description = "16 oz. bag  of red hot gummi bears", Cost = 3 , ImageUrl = "https://tse4.mm.bing.net/th?id=OIP.flaaR4C9-l9cdbIWbVletQHaHa&pid=15.1&P=0&w=300&h=300", AverageRating = 2.0m}
             };
 
             var reviews = new Review[]
@@ -35,9 +35,9 @@ namespace GummiBears.Data
                 new Review() { ReviewId = 6, Title = "Great Green Gummies", Author = "George", Content_Body = "These are great.  I can't stop eating them.", Rating = 5, ProductId = 3},
                 new Review() { ReviewId = 7, Title = "I Like These", Author = "Jimmy", Content_Body = "Gummies are the best.", Rating = 5, ProductId = 2},
                 new Review() { ReviewId = 8, Title = "good", Author = "Candy", Content_Body = "5 stars", Rating = 5, ProductId = 4},
-                 new Review() { ReviewId = 9, Title = "Buy these", Author = "Penny", Content_Body = "good eatin'", Rating = 4, ProductId = 5},
-                 new Review() { ReviewId = 10, Title = "Good Gummies", Author = "Lucy", Content_Body = "These are pretty good", Rating = 3, ProductId = 6},
-                 new Review() { ReviewId = 11, Title = "Yuck", Author = "Kyle", Content_Body = "These are gross", Rating = 1, ProductId = 6},
+                new Review() { ReviewId = 9, Title = "Buy these", Author = "Penny", Content_Body = "good eatin'", Rating = 4, ProductId = 5},
+                new Review() { ReviewId = 10, Title = "Good Gummies", Author = "Lucy", Content_Body = "These are pretty good", Rating = 3, ProductId = 6},
+                new Review() { ReviewId = 11, Title = "Yuck", Author = "Kyle", Content_Body = "These are gross", Rating = 1, ProductId = 6},
             };
 
             foreach (Product p in products)
